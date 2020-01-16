fc55a197516c5c23ca52776aca6150f8b193b0a8
deer <rui.deng@baozun.com>
2020-01-16 18:43:00 +0800

    add funtion isblank

--- __mocks__/globalVariable.js
+++ __mocks__/globalVariable.js
@@ -3,6 +3,18 @@ const variable = {
     aa:11,
     bb:22
   }],
+  isBlank: function(String str) {
+    var strLen;
+    if (str == null || (strLen = str.length()) == 0) {
+        return true;
+    }
+    for (var i = 0; i < strLen; i++) {
+        if ((Character.isWhitespace(str.charAt(i)) == false)) {
+            return false;
+        }
+    }
+    return true;
+}
   base: "https://stage.samsungeshop.com.cn/"
 }
 

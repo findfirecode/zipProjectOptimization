938858fc979d5c8beb5f74a96a17f1ec29e09b0d
deer <rui.deng@baozun.com>
2020-03-03 16:20:51 +0800

    本地跨域处理

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -5,7 +5,18 @@ config.devServer = {
   historyApiFallback: true,
   open: true,  //自动打开浏览器
   contentBase: path.join(__dirname, '../build'),
-  port: 3000
+  port: 3000,
+  // host: "10.110.6.156",
+  proxy: {
+    '/api': {
+      target: "https://stage.samsungeshop.com.cn", //开发环境
+      changeOrigin: true,
+      secure: false,
+      pathRewrite: {
+        '^/api': ''
+      }
+    }
+  }
 }
 
 config.devtool = 'inline-source-map'
